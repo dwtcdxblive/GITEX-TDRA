0871afa67803ab9ec48ce3446b0938f5a4aff30e
dwtcdxblive <omar.aouf@dwtc.com>
2023-09-22 17:00:54 +0400

    koalendar embedded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <iframe
+        src='https://koalendar.com/e/presentation-area?embed=true'
+        width='100%'
+        height='800px'
+        frameborder='0'
+      ></iframe>
     </div>
   );
 }
